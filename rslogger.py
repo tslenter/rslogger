8d3448d92b224ef59aabf8201808a77e345fceb7
Tom Slenter <tomslenter@gmail.com>
2021-07-05 17:05:44 +0200

    Update rslogger.py

--- rslogger.py
+++ rslogger.py
@@ -49,8 +49,8 @@ def syslog(message='Test is RS test message to localhost', level=lvl['notice'], 
 
 #Run the function:
 #Run test to local host as default
-syslog()
+#syslog()
 
 #Run test to server
-message=str('Hello world')
-syslog(message, level=lvl['notice'], facility=fcl['log_audit'], host='172.16.201.2', port=514)
+#message=str('Hello world')
+#syslog(message, level=lvl['notice'], facility=fcl['log_audit'], host='172.16.201.2', port=514)
